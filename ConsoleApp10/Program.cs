34e5a3f9de91020b308e2c3a5cc6b985681b7769
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:55:28 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta;
+            Console.WriteLine("Podaj a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            delta = ((b * b) - 4 * a * c);
+            Console.WriteLine(delta);
+            Console.ReadKey();
+        }
+    }
+}
